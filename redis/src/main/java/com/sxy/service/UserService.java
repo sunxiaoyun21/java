a448bd6cef978f0951d698b920faaa5834886bc5
sunxiaoyun21 <479338314@qq.com>
2017-03-05 21:29:30 +0800

    将对象转化为二进制 protostuff的用法

--- redis/src/main/java/com/sxy/service/UserService.java
+++ redis/src/main/java/com/sxy/service/UserService.java
@@ -1,6 +1,10 @@
 package com.sxy.service;
 
 import com.sxy.pojo.User;
+import io.protostuff.LinkedBuffer;
+import io.protostuff.ProtobufIOUtil;
+import io.protostuff.Schema;
+import io.protostuff.runtime.RuntimeSchema;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.BoundValueOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -18,11 +22,37 @@ import redis.clients.jedis.Jedis;
 @Service
 public class UserService {
 
-
-
+  @Autowired
   private RedisTemplate redisTemplate;
 
-    @Autowired
+
+    public  void saveRedis(){
+        User user=new User(103,"张三",89.5F);
+        Schema<User> userSchema= RuntimeSchema.getSchema(User.class);
+       //user对象转换为二进制数组
+        byte[] userByte=
+                ProtobufIOUtil.toByteArray(user,userSchema,
+                        LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+
+        redisTemplate.opsForValue().set("user:103".getBytes(),userByte);
+    }
+
+    /*
+    * 进行反序列化
+    * */
+    public String getFromRedis() {
+        Schema<User> userSchema= RuntimeSchema.getSchema(User.class);
+        User user=new User();
+        byte[] userByte= (byte[]) redisTemplate.opsForValue().get("user:103".getBytes());
+        ProtobufIOUtil.mergeFrom(userByte,user,userSchema);
+        System.out.println(user);
+        return null;
+
+       }
+
+
+
+  /*  @Autowired
     public void setRedisTemplate(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
         redisTemplate.setKeySerializer(new StringRedisSerializer());
@@ -33,14 +63,10 @@ public class UserService {
         User user=new User(101,"tom",89.5F);
         redisTemplate.opsForValue().set("name:1",user);
 
-
-
-
-
-        /*BoundValueOperations operations=redisTemplate.boundValueOps("book:name:1");
-        operations.set("redis");*/
+        *//*BoundValueOperations operations=redisTemplate.boundValueOps("book:name:1");
+        operations.set("redis");*//*
         //redisTemplate.opsForList().leftPush("book","sql","java");
-      /*  redisTemplate.opsForValue().set("book:name:2","head first java");*/
+      *//*  redisTemplate.opsForValue().set("book:name:2","head first java");*//*
     }
 
     public String getFromRedis(){
@@ -49,7 +75,7 @@ public class UserService {
         return null;
     }
 
-
+*/
 
 
 
