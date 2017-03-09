e585f0504177f5afe8fbdf847172480212cce44b
sunxiaoyun21 <479338314@qq.com>
2017-03-09 17:24:31 +0800

    activiti与spring的结合配置

---
+++ ssm/src/main/java/com/sxy/listner/LeaveBackTaskListnerImpl.java
@@ -0,0 +1,16 @@
+package com.sxy.listner;
+
+import org.activiti.engine.delegate.DelegateTask;
+import org.activiti.engine.delegate.TaskListener;
+
+/**
+ * Created by Administrator on 2017/3/9.
+ */
+public class LeaveBackTaskListnerImpl implements TaskListener{
+
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+
+    }
+}
