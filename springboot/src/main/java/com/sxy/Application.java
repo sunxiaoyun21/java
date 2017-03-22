c854a008a3e822155a53c4c116738b3bcac4f1fe
sunxiaoyun21 <479338314@qq.com>
2017-03-22 11:16:26 +0800

    springBoot的启动以及thymeleaf的使用

---
+++ springboot/src/main/java/com/sxy/Application.java
@@ -0,0 +1,21 @@
+package com.sxy;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+/*@Configuration
+@ComponentScan
+@EnableAutoConfiguration*/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
---
+++ springboot/src/main/java/com/sxy/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.sxy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/3/22.
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model){
+        model.addAttribute("message","hello,你好");
+
+        String name="jack";
+        List<String> names= Arrays.asList("aa","bb","cc");
+        model.addAttribute("name",name);
+        model.addAttribute("names",names);
+
+        return "index";
+    }
+}
